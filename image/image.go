6a0a354581d9885edd5525f79d6d5e4843568329
周子童 <zhouzitong@artron.net>
2021-10-20 11:49:13 +0800

    add

---
+++ image/image.go
@@ -0,0 +1,21 @@
+package image
+
+import (
+	"image"
+	"net/http"
+	"github.com/disintegration/imageorient"
+)
+//image width= img.Bounds().Dx()
+//image height= img.Bounds().Dy()
+func GetImage(url string) (image.Image, error) {
+	resp, err := http.Get(url)
+	defer resp.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+	img, _, err := imageorient.Decode(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return img, nil
+}
