5929658d1e3d0e34872499963683d355e7850251
周子童 <zhouzitong@artron.net>
2021-10-28 10:57:50 +0800

    error

---
+++ error/error.go
@@ -0,0 +1,22 @@
+package error
+
+import (
+	"bytes"
+	"fmt"
+	"runtime"
+)
+
+// 打印堆栈信息
+//print error stack
+func printStackTrace(err interface{}) string {
+	buf := new(bytes.Buffer)
+	fmt.Fprintf(buf, "%v\n", err)
+	for i := 1; ; i++ {
+		pc, file, line, ok := runtime.Caller(i)
+		if !ok {
+			break
+		}
+		fmt.Fprintf(buf, "%s:%d (0x%x)\n", file, line, pc)
+	}
+	return buf.String()
+}
