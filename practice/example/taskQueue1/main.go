b14760493753294629ad83fa85079c5c2a70b261
周子童 <zhouzitong@artron.net>
2021-11-04 17:36:38 +0800

    queue

--- practice/example/taskQueue1/main.go
+++ practice/example/taskQueue1/main.go
@@ -8,11 +8,14 @@ import (
 type Job struct {
 	Id int `json:"id"`
 }
+//taskqueue
 func main()  {
 	var wg sync.WaitGroup
 	wg.Add(1)
 	jobChan:=make(chan Job,100)
+	//create one worker
 	go worker(jobChan,&wg)
+	//produce job
 	for i:=0;i<100;i++{
 		if i==69{
 			close(jobChan)
@@ -25,6 +28,7 @@ func main()  {
 }
 func worker(jobChan <-chan Job,wg *sync.WaitGroup)  {
 	defer wg.Done()
+	//handle work
 	for job:=range jobChan{
 		process(job)
 	}
