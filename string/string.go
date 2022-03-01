73a425318a514b75e1ee90f724d6579a752c99ef
周子童 <zhouzitong@artron.net>
2022-03-01 15:55:29 +0800

    add

--- string/string.go
+++ string/string.go
@@ -1,6 +1,9 @@
 package string
 
-import "strings"
+import (
+	"regexp"
+	"strings"
+)
 //fomat 12345678 to 12,345,678
 func NumFormat(str string) string {
 	numStr := strings.Split(str, ".")[0] //如果有小数获取整数部分
@@ -13,4 +16,32 @@ func NumFormat(str string) string {
 		numStr = numStr[:length-(i+1)*3] + "," + numStr[length-(i+1)*3:]
 	}
 	return numStr
+}
+//filter string html css js script
+func ClearXssSql(src string)(res string)  {
+	src = strings.TrimSpace(src) //去空格
+	//将HTML标签全转换成小写
+	re, _ := regexp.Compile("\\<[\\S\\s]+?\\>")
+	src = re.ReplaceAllStringFunc(src, strings.ToLower)
+
+	//去除STYLE
+	re, _ = regexp.Compile("\\<style[\\S\\s]+?\\</style\\>")
+	src = re.ReplaceAllString(src, "")
+
+	//去除SCRIPT
+	re, _ = regexp.Compile("\\<script[\\S\\s]+?\\</script\\>")
+	src = re.ReplaceAllString(src, "")
+
+	//去除所有尖括号内的HTML代码，并换成换行符
+	re, _ = regexp.Compile("\\<[\\S\\s]+?\\>")
+	src = re.ReplaceAllString(src, "\n")
+
+	//去除连续的换行符
+	re, _ = regexp.Compile("\\s{2,}")
+	src = re.ReplaceAllString(src, "\n")
+
+	re, _ = regexp.Compile("&nbsp;")
+	src = re.ReplaceAllString(src, "")
+	res=src
+	return
 }
