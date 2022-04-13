cc78c579eb6ee48dd4f84821965fba3e8632e8b7
周子童 <zhouzitong@artron.net>
2022-04-13 16:44:20 +0800

    debug

--- error/error.go
+++ error/error.go
@@ -3,7 +3,9 @@ package error
 import (
 	"bytes"
 	"fmt"
+	"log"
 	"runtime"
+	"runtime/debug"
 )
 
 // 打印堆栈信息
@@ -19,4 +21,25 @@ func PrintStackTrace(err interface{}) string {
 		_, _ = fmt.Fprintf(buf, "%s:%d (0x%x)\n", file, line, pc)
 	}
 	return buf.String()
+}
+func Debug1()  {
+	//1. using runtime
+	if p := recover(); p != nil {
+		log.Printf("message push panic: %v", p)
+
+		//打印调用栈信息
+		buf := make([]byte, 2048)
+		n := runtime.Stack(buf, false)
+		stackInfo := fmt.Sprintf("%s", buf[:n])
+		log.Printf("panic stack info %s", stackInfo)
+	}
+
+}
+func Debug2()  {
+	if p := recover(); p != nil {
+		log.Printf("message push panic: %v", p)
+		//打印调用栈信息
+		debug.PrintStack()
+	}
+
 }
