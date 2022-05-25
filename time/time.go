104246c684bc84540d5c1d173d7bdae4d2e6ce05
周子童 <zhouzitong@artron.net>
2022-05-25 15:48:29 +0800

    time

--- time/time.go
+++ time/time.go
@@ -7,20 +7,24 @@ import "time"
 //timeTemplate2 := "2006/01/02 15:04:05" //其他类型
 //timeTemplate3 := "2006-01-02"          //其他类型
 //timeTemplate4 := "15:04:05"            //其他类型
-func TimeStrToTimeStamp(t1 string,timeType int) int64 {
+func TimeStrToTimeStamp(t1 string, timeType int) int64 {
 
 	timeTemplate := "2006-01-02 15:04:05" //常规类型
-	if timeType==1{
+	if timeType == 1 {
 		timeTemplate = "2006-01-02 15:04:05" //常规类型
-	} else if timeType==2{
+	} else if timeType == 2 {
 		timeTemplate = "2006/01/02 15:04:05" //常规类型
-	} else if timeType==3{
+	} else if timeType == 3 {
 		timeTemplate = "2006-01-02" //常规类型
-	} else if timeType==4{
+	} else if timeType == 4 {
 		timeTemplate = "15:04:05" //常规类型
-	}else {
+	} else {
 		timeTemplate = "2006-01-02 15:04:05" //常规类型
 	}
 	stamp, _ := time.ParseInLocation(timeTemplate, t1, time.Local) //使用parseInLocation将字符串格式化返回本地时区时间
 	return stamp.Unix()
+}
+////单位s,打印结果:1491888244
+func TimeUnixStampSecond() int64 {
+	return time.Now().Unix()
 }
