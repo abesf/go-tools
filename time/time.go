47184604200c0c856a204ac49961f7edad419884
周子童 <zhouzitong@artron.net>
2022-05-26 10:22:12 +0800

    tim

--- time/time.go
+++ time/time.go
@@ -24,7 +24,28 @@ func TimeStrToTimeStamp(t1 string, timeType int) int64 {
 	stamp, _ := time.ParseInLocation(timeTemplate, t1, time.Local) //使用parseInLocation将字符串格式化返回本地时区时间
 	return stamp.Unix()
 }
-////单位s,打印结果:1491888244
-func TimeUnixStampSecond() int64 {
+
+// TimeNowUnixStampSecond Time Now to UnixStamp (Second) 单位s,打印结果:1491888244
+func TimeNowUnixStampSecond() int64 {
 	return time.Now().Unix()
+}
+
+// TimeStampToTimeStr  TimeStamp To TimeStr
+func TimeStampToTimeStr(timestamp int64,timeType int) string {
+
+	timeTemplate := "2006-01-02 15:04:05" //常规类型
+	if timeType==1{
+		timeTemplate = "2006-01-02 15:04:05" //常规类型
+	} else if timeType==2{
+		timeTemplate = "2006/01/02 15:04:05" //常规类型
+	} else if timeType==3{
+		timeTemplate = "2006-01-02" //常规类型
+	} else if timeType==4{
+		timeTemplate = "15:04:05" //常规类型
+	}else {
+		timeTemplate = "2006-01-02 15:04:05" //常规类型
+	}
+	timeNow:=time.Unix(timestamp, 0)
+	timeString := timeNow.Format(timeTemplate) //2015-06-15 08:52:32
+	return timeString
 }
